140d30082642422383b9016817aba92026fb35c8
DevSatpathy <devyesh2@illinois.edu>
2019-03-11 19:57:26 -0500

    added map component mock

---
+++ pages/startupfair.jsx
@@ -0,0 +1,40 @@
+// import { Component } from 'react';
+// import ImageMapper from 'react-image-mapper';
+/*
+const MAP = {
+  name: 'my-map',
+  areas: [
+    {
+      name: 'Granular',
+      shape: 'poly',
+      coords: [7, 400, 7, 435, 143, 435, 143, 400],
+      fillColor: 'rgba(200, 0, 0, 0.3)'
+    },
+
+    // preFillColor: 'green', fillColor: 'blue'},
+    {
+      name: 'Microsoft',
+      shape: 'poly',
+      coords: [133, 200, 133, 230, 270, 230, 270, 200],
+      href: 'https://www.microsoft.com/en-us/about'
+    }, // preFillColor: 'pink'},
+    // {name: '3', shape: 'poly', coords: [381,241,383,94,462,53,457,282], fillColor: 'yellow'},
+    {
+      name: 'Quora',
+      shape: 'poly',
+      coords: [282, 195, 282, 230, 389, 230, 389, 195],
+      href: 'https://www.quora.com/about'
+    }
+  ]
+};
+
+const URL =
+  'https://raw.githubusercontent.com/ReflectionsProjections/rp2018/master/assets/files/career_fair.png';
+
+ const mapcomp = <ImageMapper src={URL} map={MAP} width={1080} />;
+*/
+const Map = () => {
+  return null;
+};
+
+export default Map;
